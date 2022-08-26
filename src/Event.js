1ba9b1956f9b650af34287c4ec18048566e8428d
Sky <skyrj50031@maricopa.edu>
2022-08-25 20:49:40 -0700

    Update Event.js
    Added more to events

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,40 @@
 import React, { Component } from "react";
+import { mockData } from './mock-data';
 
 class Event extends Component {
+  state = {
+		collapsed: true,
+	};
+
+	showSummary = () => {
+		if (this.state.collapsed === false) {
+			return mockData[0].description;
+		}
+	};
+
+	handleClick = () => {
+		this.setState({
+			collapsed: !this.state.collapsed
+		});
+	};
+
   render() {
-    return <div></div>;
+    const { collapsed } = this.state;
+    const { event } = this.props;
+		return (
+			<div className="event">
+				<h3 className="title">{event.summary}</h3>
+				<p className="start-time">
+					{event.start.dateTime} {event.start.timeZone}
+				</p>
+				<p className="location">{event.location}</p>
+				<button className={`btn-details ${collapsed ? 'show' : 'hide'}-details`} onClick={this.handleClick}>
+					{collapsed ? 'Show Details' : 'Hide Details'}
+				</button>
+				{!collapsed &&
+					<div className={`event-details ${this.state.collapsed ? 'hide' : 'show'}`}>{this.showSummary()}</div>}
+			</div>
+		);
   }
 }
 export default Event;
