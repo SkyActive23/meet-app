d4c544b60c89761d4fd92f63d17351754bc7a380
Sky <skyrj50031@maricopa.edu>
2022-09-19 19:39:56 -0700

    4.7

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,6 +1,7 @@
 // src/CitySearch.js
 
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
     state = {
@@ -14,16 +15,26 @@ class CitySearch extends Component {
         const suggestions = this.props.locations.filter((location) => {
           return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         });
-        this.setState({
-          query: value,
-          suggestions,
-        });
+        if (suggestions.length === 0){
+            this.setState({
+                query: value,
+                infoText: 'We can not find the city you are looking for. Please try another city',
+            });
+        } else {
+            return this.setState({
+                query: value,
+                suggestions,
+                infoText: ''
+            });
+        }
     };
 
     handleItemClicked = (suggestion) => {
         this.setState({
           query: suggestion,
-          showSuggestions: false
+          suggestions:[],
+          showSuggestions: false,
+          infoText: ''
         });
         this.props.updateEvents(suggestion);
     }
@@ -31,7 +42,8 @@ class CitySearch extends Component {
 
     render() {
         return (
-            <div className="CitySearch">
+            <div className="CitySearch" >
+                <InfoAlert text={this.state.infoText} />
                 <input
                     type="text"
                     className="city"
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert'; 
 
 class NumberOfEvents extends Component {
 	state = {
 		numberOfEvents: 32,
+		infoText: ''
 	};
 
 	handleInputChanged = (event) => {
 		const value = event.target.value;
-		if (!isNaN(value) && value >= 1) {
-			return this.setState({ numberOfEvents: value });
+		if (value <= 0 || value > 32 ) {
+			this.setState({
+				numberOfEvents:'',
+				infoText: 'Please pick a number 1-32'
+			})
 		} else {
-			console.log('Please enter a valid number.');
-			return this.state.numberOfEvents;
+			this.setState({
+				numberOfEvents: value,
+				infoText: ''
+			})
 		}
-	};
+	}
 
 	render() {
 		return (
 			<div className='numberOfEvents'>
+				<ErrorAlert text = {this.state.infoText}/>
 				<label for="events-number">Number of Events</label>
 				<input
 					type="text"
