c065ac98c92233a495a194591eeb304d230745df
Sky <skyrj50031@maricopa.edu>
2022-09-25 20:35:43 -0700

    4.9 changes
    Added EventGenre with pie chart
    Added styles to charts
    Imported EventGenre
    deleted package-lock.json

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from "./NumberOfEvents";
-import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
-import WelcomeScreen from './WelcomeScreen';
+import { EventGenre } from './EventGenre';
+import { getEvents, extractLocations } from './api';
+// import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+// import WelcomeScreen from './WelcomeScreen';
 import Card from 'react-bootstrap/Card'
 
 import './nprogress.css';
@@ -14,7 +17,7 @@ class App extends Component {
     events: [],
     locations: [],
     numberOfEvents: 15,
-    showWelcomeScreen: undefined
+    // showWelcomeScreen: undefined
   }
 
   updateEvents = (location, eventCount) => {
@@ -41,28 +44,49 @@ class App extends Component {
     this.updateEvents(this.state.locations, numberOfEvents);
   };
 
-  async componentDidMount() {
+  // async componentDidMount() {
+  //   this.mounted = true;
+  //   const accessToken = localStorage.getItem('access_token');
+  //   const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+  //   const searchParams = new URLSearchParams(window.location.search);
+  //   const code = searchParams.get("code");
+  //   this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+  //   if ((code || isTokenValid) && this.mounted) {
+  //     getEvents().then((events) => {
+  //       if (this.mounted) {
+  //         this.setState({ events, locations: extractLocations(events) });
+  //       }
+  //     });
+  //   }
+  // }
+
+  componentDidMount() {
     this.mounted = true;
-    const accessToken = localStorage.getItem('access_token');
-    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get("code");
-    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    if ((code || isTokenValid) && this.mounted) {
-      getEvents().then((events) => {
-        if (this.mounted) {
-          this.setState({ events, locations: extractLocations(events) });
-        }
-      });
-    }
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
   }
+   
 
   componentWillUnmount(){
     this.mounted = false;
   }
 
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    })
+    return data;
+  };
+
+
   render () {
-    if (this.state.showWelcomeScreen === undefined) return <div className='App' />
+    // if (this.state.showWelcomeScreen === undefined) return <div className='App' />
 
     return (
       <div className="App">
@@ -71,11 +95,30 @@ class App extends Component {
           numberOfEvents={this.state.numberOfEvents}
           updateNumberOfEvents={this.updateNumberOfEvents} />
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
+        <div>
+          <Card className='charts'>
+            <h4>Events in each city</h4>
+            <ResponsiveContainer height={400} >
+              <ScatterChart
+                margin={{
+                  top: 20, right: 20, bottom: 20, left: 20,
+                }}
+              >
+                <CartesianGrid />
+                <XAxis type="category" dataKey="city" name="city" />
+                <YAxis allowDecimals={false} type="number" dataKey="number" name="number of events" />
+                <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                <Scatter data={this.getData()} fill="#8884d8" />
+              </ScatterChart>
+            </ResponsiveContainer>
+            <EventGenre events={this.state.events} />
+          </Card>
+        </div>
         <Card>
           <EventList events={this.state.events} />
         </Card>
-        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
-          getAccessToken={() => { getAccessToken() }} />
+        {/* <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => { getAccessToken() }} /> */}
       </div>
     )
   } 
---
+++ src/EventGenre.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+
+export const EventGenre = ({ events }) => {
+
+	const [data, setData] = useState([]);
+	useEffect(() => {
+		const getData = () => {
+			const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+			const data = genres.map((genre) => {
+				const value = events.filter((event) => event.summary.split(' ').includes(genre)).length;
+				return { name: genre, value };
+			});
+			return data;
+		};
+		setData(() => getData());
+	}, [events]);
+
+	const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#FF66B2'];
+
+	return (
+		<>
+			<h4 >Distribution of event type</h4>
+			<ResponsiveContainer height={300}>
+				<PieChart>
+					<Pie
+						data={data}
+						cx="50%"
+						cy="50%"
+						labelLine={false}
+						outerRadius={80}
+						fill='rgb(4, 67, 137)'
+						datakey='value'
+						label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+					>{
+							data.map((entry, index) => (<Cell key={index} fill={COLORS[index]} />
+							))}
+					</Pie>
+				</PieChart>
+			</ResponsiveContainer>
+		</>
+	);
+
+}
